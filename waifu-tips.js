3e0e34cb300bc9ed7969e23ab47b448755d0f1b4
dbconf <76120082+dbconf@users.noreply.github.com>
2021-02-24 06:51:58 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -53,18 +53,18 @@ live2d_settings['showWelcomeMessage']   = true;         // 显示进入面页欢
 
 //看板娘样式设置
 live2d_settings['waifuSize']            = '330x330';    // 看板娘大小，例如 '280x250', '600x535'
-live2d_settings['waifuTipsSize']        = '250x70';     // 提示框大小，例如 '250x70', '570x150'
+live2d_settings['waifuTipsSize']        = '250x60';     // 提示框大小，例如 '250x70', '570x150'
 live2d_settings['waifuFontSize']        = '14px';       // 提示框字体，例如 '12px', '30px'
 live2d_settings['waifuToolFont']        = '14px';       // 工具栏字体，例如 '14px', '36px'
 live2d_settings['waifuToolLine']        = '20px';       // 工具栏行高，例如 '20px', '36px'
 live2d_settings['waifuToolTop']         = '0px'         // 工具栏顶部边距，例如 '0px', '-60px'
-live2d_settings['waifuMinWidth']        = '500px';      // 面页小于 指定宽度 隐藏看板娘，例如 'disable'(禁用), '768px'
+live2d_settings['waifuMinWidth']        = '400px';      // 面页小于 指定宽度 隐藏看板娘，例如 'disable'(禁用), '768px'
 live2d_settings['waifuEdgeSide']        = 'right:20';     // 看板娘贴边方向，例如 'left:0'(靠左 0px), 'right:30'(靠右 30px)
-live2d_settings['waifuDraggable']       = 'axis-x';    // 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽), 'unlimited'(自由拖拽)
+live2d_settings['waifuDraggable']       = 'unlimited';    // 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽), 'unlimited'(自由拖拽)
 live2d_settings['waifuDraggableRevert'] = false;         // 松开鼠标还原拖拽位置，可选 true(真), false(假)
 
 // 其他杂项设置
-live2d_settings['l2dVersion']           = '1.0.5';        // 当前版本
+live2d_settings['l2dVersion']           = '1.0.6';        // 当前版本
 live2d_settings['l2dVerDate']           = '2021.1.12'; // 版本更新日期
 live2d_settings['homePageUrl']          = 'auto';       // 主页地址，可选 'auto'(自动), '{URL 网址}'
 live2d_settings['aboutPageUrl']         = 'https://dabai.in/';   // 关于页地址, '{URL 网址}'
