10fe3362bce4edef16fa30c4aa41b13a0b969709
dbconf <76120082+dbconf@users.noreply.github.com>
2021-02-24 03:43:30 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,9 +1,9 @@
 try {
-    $("<link>").attr({href: "/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $("<link>").attr({href: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@latest/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
     
-    $.ajax({url: "/waifu-tips.min.js", dataType:"script", cache: true, success: function() {
-        $.ajax({url: "/live2d.min.js", dataType:"script", cache: true, success: function() {
+    $.ajax({url: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@latest/waifu-tips.min.js", dataType:"script", cache: true, success: function() {
+        $.ajax({url: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@latest/live2d.min.js", dataType:"script", cache: true, success: function() {
             /* 可直接修改部分参数 */
             
             live2d_settings['modelId'] = 6;                  // 默认模型 ID
@@ -11,7 +11,7 @@ try {
             live2d_settings['modelStorage'] = false;         // 不储存模型 I
             
              /*  在 initModel 前添加 */
-            initModel("/waifu-tips.min.json");
+            initModel("https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@latest/waifu-tips.min.json");
         }});
     }});
 } catch(err) { console.log("[Error] JQuery is not defined.") }
