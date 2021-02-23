e26b0bc48d89004efa301787b0e2f470392ce5ed
dbconf <76120082+dbconf@users.noreply.github.com>
2021-02-24 01:19:32 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -2,7 +2,7 @@ try {
     $("<link>").attr({href: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@latest/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
     
-    $.ajax({url: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@1.0.12/waifu-tips.js", dataType:"script", cache: true, success: function() {
+    $.ajax({url: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@1.0.13/waifu-tips.js", dataType:"script", cache: true, success: function() {
         $.ajax({url: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@latest/live2d.js", dataType:"script", cache: true, success: function() {
             /* 可直接修改部分参数 */
             
