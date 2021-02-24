0051002507da96319c46cacd7c9c54e299a10694
dbconf <76120082+dbconf@users.noreply.github.com>
2021-02-25 05:17:03 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,9 +1,9 @@
 try {
-    $("<link>").attr({href: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@latest/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $("<link>").attr({href: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
     
-    $.ajax({url: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@latest/waifu-tips.min.js", dataType:"script", cache: true, success: function() {
-        $.ajax({url: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@latest/live2d.min.js", dataType:"script", cache: true, success: function() {
+    $.ajax({url: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud/waifu-tips.min.js", dataType:"script", cache: true, success: function() {
+        $.ajax({url: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud/live2d.min.js", dataType:"script", cache: true, success: function() {
             /* 可直接修改部分参数 
             
             live2d_settings['modelAPI']             = '//live2d.fghrsh.net/api/';   // 自建 API 修改这里
@@ -15,7 +15,7 @@ try {
             live2d_settings['waifuDraggable']       = 'axis-x';       // 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽), 'unlimited'(自由拖拽)
             
                在 initModel 前添加 */
-            initModel("https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@latest/waifu-tips.json");
+            initModel("https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud/waifu-tips.json");
         }});
     }});
 } catch(err) { console.log("[Error] JQuery is not defined.") }
