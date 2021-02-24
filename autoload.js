27acb5ac93bcbc686c94090937364d21c4754c8d
dbconf <76120082+dbconf@users.noreply.github.com>
2021-02-24 21:18:29 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -6,9 +6,13 @@ try {
         $.ajax({url: "https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@latest/live2d.min.js", dataType:"script", cache: true, success: function() {
             /* 可直接修改部分参数 */
             
-            live2d_settings['modelId'] = 6;                  // 默认模型 ID
-            live2d_settings['modelTexturesId'] = 2;          // 默认材质 ID
-            live2d_settings['modelStorage'] = false;         // 不储存模型 I
+            live2d_settings['modelAPI']             = '//live2d.fghrsh.net/api/';   // 自建 API 修改这里
+            live2d_settings['modelId']              = 6;                 // 默认模型 ID
+            live2d_settings['modelTexturesId']      = 2;                 // 默认材质 ID
+            live2d_settings['modelStorage']         = false;             // 不储存模型 I
+            live2d_settings['waifuMinWidth']        = '400px';           // 面页小于 指定宽度 隐藏看板娘，例如 'disable'(禁用), '768px'
+            live2d_settings['waifuEdgeSide']        = 'left:130';        // 看板娘贴边方向，例如 'left:0'(靠左 0px), 'right:30'(靠右 30px)
+            live2d_settings['waifuDraggable']       = 'unlimited';       // 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽), 'unlimited'(自由拖拽)
             
              /*  在 initModel 前添加 */
             initModel("https://cdn.jsdelivr.net/gh/dbconf/cdn-cloud@latest/waifu-tips.json");
